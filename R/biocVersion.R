ffc9e80435d4277febc73c9e1c0360168d9d4028
LiNk-NY <mramos09@gmail.com>
2018-03-07 19:42:37 -0500

    update biocVersion function

--- R/biocVersion.R
+++ R/biocVersion.R
@@ -1,4 +1,19 @@
-biocVersion <- function(version = "3.7") {
-    .options$set(biocVersion = numeric_version(version))
+#' Bioconductor version
+#'
+#' This function reports the Bioconductor version in use, as determined by
+#' the version of the BiocVersion package.
+#'
+#' @return \code{package_version} representing the Bioconductor version in use.
+#' @seealso
+#'
+#' \code{\link{install}} Installs/updates Bioconductor/CRAN packages.
+#'
+#' @keywords environment
+#' @examples
+#'
+#' biocVersion()
+#'
+#' @export biocVersion
+biocVersion <- function() {
+    utils::packageVersion("BiocVersion")[1, 1:2]
 }
-
